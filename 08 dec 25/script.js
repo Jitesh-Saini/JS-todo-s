fd3949966b53d3b08e2cdfd56f09790262354a56
jitesh <iamjiteshsaini2004@gmail.com>
2025-12-09 22:08:57 +0530

    first commit

---
+++ 08 dec 25/script.js
@@ -0,0 +1,31 @@
+// 1. Print numbers from 1 to 100 using a loop.
+
+// for (let i = 1; i <= 100; i++) {
+//   console.log(i);
+// }
+
+// 2. Print all even numbers between 1 and 50.
+
+// for (let i = 1; i <= 50; i++) {
+//   if (i % 2 == 0) console.log(i);
+// }
+
+// 3. Print all odd numbers between 1 and 50.
+
+// for (let i = 1; i <= 50; i++) {
+//   if (i % 2 != 0) console.log(i);
+// }
+
+// 4. Print numbers from 50 to 1 in reverse order.
+
+// for (let i = 50; i >= 1; i--) {
+//    console.log(i);
+// }
+
+// 5. Find the sum of numbers from 1 to 100.
+
+// let sum = 0;
+// for (let i = 1; i <= 100; i++) {
+//   sum += i;
+// }
+// console.log(sum);
